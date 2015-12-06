cb8190fa1c201cc14f9e6040c4c29f7cb9ce05ae
benjaminyhl <yehaolan@gmail.com>
2015-12-05 16:31:25 -0800

    yehaolan@gmail.com

--- Hmwk/Tree/Tree.h
+++ Hmwk/Tree/Tree.h
@@ -23,6 +23,7 @@ class Tree {
         Node *root;   //Root node of the tree
        int size;      // Actual Size of the array
        void insert(Node *&,Node *&); //insert
+       void dele(Node *&,int);
        int getBalance(Node *);//balance the tree       
        //function for printing
        void preOrder(Node *); 
@@ -42,7 +43,7 @@ class Tree {
        Tree();   //Default constructor  
        ~Tree();  // Destructor declaration
        void insert(int); //insert a number to a tree
-       void del(int);//delete
+       void dele(int);//delete
        void prePnt(); //print out tree with pre-order
        void inPnt(); //print out tree with in-order
        void postPnt(); //print out tree with post-order
@@ -97,19 +98,19 @@ void Tree::insert(Node *&node, Node *&newNode) {
     //Right case
     if(b<-1) {
         if(newNode->value >= node->right->value) {
-            cout<<"RR case "<<"at "<<node->value<<endl;
+//            cout<<"RR case "<<"at "<<node->value<<endl;
             rotateRR(node);
         } else {
-            cout<<"RL case "<<"at "<<node->value<<endl;
+//            cout<<"RL case "<<"at "<<node->value<<endl;
             rotateRL(node);
         }
     }
     if(b>1) {
         if(newNode->value <= node->left->value) {
-            cout<<"LL case "<<"at "<<node->value<<endl;
+//            cout<<"LL case "<<"at "<<node->value<<endl;
             rotateLL(node);
         } else {
-            cout<<"LR case "<<"at "<<node->value<<endl;
+//            cout<<"LR case "<<"at "<<node->value<<endl;
             rotateLR(node);
         }
     }
@@ -176,8 +177,80 @@ void Tree::rotateRL(Node *&z) {
 }
 
 //delete a tree
-void Tree::del(int n) {
+void Tree::dele(int n) {
+    dele(root,n);
+    //size --
+}
+
+void Tree::dele(Node *&node,int n) {
+    if(node==NULL) return;
+    if(n < node->value) {
+        //The node to be deleted on the left side
+        dele(node->left,n);
+    } else if( n > node->value) {
+        //The node to be deleted on the right side
+        dele(node->right,n);
+    } else {
+        //This is the node to be deleted
+        //This node has one child or no child case
+        if((node->left==NULL) || (node->right==NULL)) {
+            //no child
+            if((node->left==NULL) && (node->right==NULL)) {
+                Node *temp=node;
+                node=NULL;
+                delete temp;
+            } else {
+                //assign the child to temp
+                Node *temp= node->left ? node->left : node->right;
+                Node *temp2=node;
+                node=temp;
+                delete temp2;
+            }
+        } else { //the node to be deleted has two children
+            
+            //Get the node contains max value from the left node
+//            cout<<"Here Here "<<node->value<<endl;
+//            Node *temp=maxValueNode(node->left);
+            Node *temp=node->left;
+            if(temp!=NULL) {
+                while(temp->right!=NULL) {
+                    temp=temp->right;
+                }
+            }
+//            cout<<"Temp valueaaaa = "<<temp->value<<endl;
+            node->value=temp->value;
+//            cout<<"Node valueaaaa = "<<node->value<<endl;
+            dele(node->left,node->value);
+        }
+        
+        size--;//tree node size -1
+    }
+    if(node==NULL) return;
+    node->height=max(getHeight(node->left),getHeight(node->right))+1;
+    int b=getBalance(node); //balance
+//    cout<<"Node is "<<node->value<<" height "<<node->height<<endl;
     
+    //Right case
+    if(b<-1) {
+        
+        if(getBalance(node->right)<=0) {
+            cout<<"RR case "<<"at "<<node->value<<endl;
+            rotateRR(node);
+        } else {
+            cout<<"RL case "<<"at "<<node->value<<endl;
+            rotateRL(node);
+        }
+    }
+    if(b>1) {
+        
+        if(getBalance(node->left)>=0) {
+            cout<<"LL case "<<"at "<<node->value<<endl;
+            rotateLL(node);
+        } else {
+            cout<<"LR case "<<"at "<<node->value<<endl;
+            rotateLR(node);
+        }
+    }
 }
 
 
--- Hmwk/Tree/main.cpp
+++ Hmwk/Tree/main.cpp
@@ -5,23 +5,33 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 #include "Tree.h"
 
 int main(int argc, char** argv) {
+    //set seed
+    srand(static_cast<unsigned int>(time(0)));
     Tree t;
-    t.insert(50);
+    t.insert(55);
     t.insert(40);
-    t.insert(30);
-    t.insert(20);
-    t.insert(10);
-    t.insert(35);
     t.insert(60);
-    t.insert(70);
+    t.insert(30);
     t.insert(45);
-//    cout<<t.getSize()<<endl;
+    t.insert(70);
+    t.insert(20);
+    t.dele(45);
+    t.dele(70);
+    t.dele(20);
+    t.dele(60);
+//    t.insert(5);
+//    t.insert(25);
+//    t.insert(22);
+    cout<<t.getSize()<<endl;
     t.inPnt();
+    
 //    t.postPnt();
 //    t.prePnt();
     return 0;
